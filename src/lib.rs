f794ac7a2104424060933c8207babd4d69b066bf
LoipesMas <46327403+LoipesMas@users.noreply.github.com>
2021-12-30 20:22:13 +0100

    Sort, list, reverse, colored output

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
+use colored::*;
 use std::fmt;
 use std::os::unix::fs::MetadataExt;
 use std::path::{Path, PathBuf};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Node {
     pub path: PathBuf,
     pub size: u64,
     pub children: Vec<Node>,
+    pub is_dir: bool,
 }
 
 impl fmt::Display for Node {
@@ -18,20 +20,30 @@ impl fmt::Display for Node {
 impl Node {
     pub fn new(path: PathBuf, size: u64, children: Vec<Node>) -> Self {
         Self {
-            path,
             size,
             children,
+            is_dir: path.is_dir(),
+            path,
         }
     }
-    pub fn get_as_string_line(&self) -> String {
-        self.path
-            .file_name()
-            .map(|f| f.to_str())
-            .flatten()
-            .unwrap_or_else(|| self.path.to_str().unwrap_or("??"))
-            .to_owned()
-            + " == "
-            + &file_size_to_str(self.size)
+    pub fn get_as_string_line(&self, full_path: bool) -> String {
+        let string = if full_path {
+            self.path.to_str().unwrap_or("??")
+        } else {
+            self.path
+                .file_name()
+                .map(|f| f.to_str())
+                .flatten()
+                .unwrap_or_else(|| self.path.to_str().unwrap_or("??"))
+        };
+        let mut string = string.to_owned();
+        let string = if self.is_dir {
+            string += &std::path::MAIN_SEPARATOR.to_string();
+            string.bright_blue()
+        } else {
+            string.cyan()
+        };
+        string.to_string() + " " + &file_size_to_str(self.size).green().to_string()
     }
     pub fn get_as_string_tree(&self, depth: usize, size_filter: Option<u64>) -> (String, bool) {
         let mut passed_filter = if let Some(size_filter) = size_filter {
@@ -39,19 +51,8 @@ impl Node {
         } else {
             true
         };
-        let mut result = "––".repeat(depth);
-        result += " ";
-        // Print full path of top node
-        if depth == 0 {
-            result += self
-                .path
-                .parent()
-                .map(|p| p.to_str())
-                .flatten()
-                .unwrap_or("");
-            result += "/";
-        }
-        result += &self.get_as_string_line();
+        let mut result = "| ".repeat(depth);
+        result += &self.get_as_string_line(depth == 0);
         result += "\n";
         for child in &self.children {
             let child_res = child.get_as_string_tree(depth + 1, size_filter);
@@ -60,7 +61,8 @@ impl Node {
                     result += &child_res.0;
                     passed_filter = true;
                 } else if child.size >= size_filter {
-                    result += &("––".repeat(depth + 1) + " " + &child.get_as_string_line() + "\n");
+                    result +=
+                        &("| ".repeat(depth + 1) + " " + &child.get_as_string_line(false) + "\n");
                     passed_filter = true;
                 }
             } else {
@@ -69,11 +71,34 @@ impl Node {
         }
         (result, passed_filter)
     }
+    pub fn flatten(&self) -> Vec<Node> {
+        let mut nodes = vec![self.clone_childless()];
+        for child in &self.children {
+            nodes.append(&mut child.flatten());
+        }
+        nodes
+    }
+    pub fn clone_childless(&self) -> Self {
+        Self {
+            path: self.path.clone(),
+            is_dir: self.is_dir,
+            size: self.size,
+            children: vec![],
+        }
+    }
+
+    pub fn sort(&mut self) {
+        self.children.sort_unstable_by_key(|c| c.size);
+        self.children.reverse();
+        for child in self.children.iter_mut() {
+            child.sort();
+        }
+    }
 }
 
 pub fn walk_dir(path: &Path, depth: i32, follow_symlinks: bool) -> Node {
     let mut nodes: Vec<Node> = vec![];
-    let mut total_size = 0;
+    let mut total_size = path.metadata().map(|m| m.size()).unwrap_or(0);
     if let Ok(entries) = path.read_dir() {
         for entry in entries.flatten() {
             if let Ok(file_type) = entry.file_type() {
@@ -97,16 +122,17 @@ pub fn walk_dir(path: &Path, depth: i32, follow_symlinks: bool) -> Node {
 }
 
 pub fn file_size_to_str(size: u64) -> String {
-    match size {
-        0..=1024 => size.to_string() + "B",
-        1025..=1048576 => {
-            format!("{:.1}KB", size as f32 / 1024.0)
+    let exp = (size as f32).log10() as u32;
+    match exp {
+        0..=2 => size.to_string() + "B",
+        3..=5 => {
+            format!("{:.1}KB", size as f32 / 1000.0)
         }
-        1048577..=1073741824 => {
-            format!("{:.1}MB", size as f32 / 1048576.0)
+        6..=8 => {
+            format!("{:.1}MB", size as f32 / 1000000.0)
         }
-        1073741825.. => {
-            format!("{:.1}GB", size as f32 / 1073741825.0)
+        9.. => {
+            format!("{:.1}GB", size as f32 / 1000000000.0)
         }
     }
 }
@@ -124,7 +150,7 @@ pub fn str_to_file_size(input: &str) -> Result<u64, String> {
             "" | "B" => 0,
             u => {
                 return Err(format!(
-                    "Invalid file size unit: {}.\n Supported filesizes: B, MB, KB, GB.",
+                    "Invalid file size unit: {}.\n Supported file size units: B, MB, KB, GB.",
                     u
                 ))
             }
@@ -140,5 +166,5 @@ pub fn str_to_file_size(input: &str) -> Result<u64, String> {
         return Err(format!("Failed to parse value: {}", input));
     }
 
-    Ok((value * 1024u64.pow(exponent) as f32) as u64)
+    Ok((value * 1000u64.pow(exponent) as f32) as u64)
 }
--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,29 @@ fn main() {
         )
         .arg(
             Arg::with_name("depth")
-                .help("Depth of displayed tree")
+                .help("Depth of displayed tree/list")
                 .long("depth")
                 .default_value("-1")
                 .takes_value(true)
                 .short("d"),
+        )
+        .arg(
+            Arg::with_name("sort")
+                .help("Sort entries by size")
+                .long("sort")
+                .short("s"),
+        )
+        .arg(
+            Arg::with_name("reverse")
+                .help("Reverse the output")
+                .long("reverse")
+                .short("r"),
+        )
+        .arg(
+            Arg::with_name("list")
+                .help("Display entries as a list instead of a tree")
+                .long("list")
+                .short("l"),
         )
         .arg(
             Arg::with_name("size filter")
@@ -30,6 +48,9 @@ fn main() {
     let depth_input = matches.value_of("depth").unwrap();
     let depth = parse_depth(depth_input);
     let path = matches.value_of("path").unwrap();
+    let sort = matches.is_present("sort");
+    let list = matches.is_present("list");
+    let reverse = matches.is_present("reverse");
     let size_filter = matches.value_of("size filter").map(|a| {
         let r = sofidu::str_to_file_size(a);
         match r {
@@ -41,8 +62,34 @@ fn main() {
         }
     });
 
-    let node = sofidu::walk_dir(Path::new(path), depth, false);
-    println!("{}", node.get_as_string_tree(0, size_filter).0);
+    let mut node = sofidu::walk_dir(Path::new(path), depth, false);
+    if sort {
+        node.sort();
+    }
+    let mut output = if list {
+        let mut output = "".to_owned();
+        let nodes = node.flatten();
+        for node in nodes {
+            if let Some(size_filter) = size_filter {
+                if node.size < size_filter {
+                    continue;
+                }
+            }
+            output += &node.get_as_string_line(true);
+            output += "\n";
+        }
+        output
+    } else {
+        node.get_as_string_tree(0, size_filter).0
+    };
+    if reverse {
+        output = output
+            .lines()
+            .rev()
+            .map(|l| l.to_owned() + "\n")
+            .collect::<String>();
+    }
+    println!("{}", output);
 }
 
 fn parse_depth(input: &str) -> i32 {
